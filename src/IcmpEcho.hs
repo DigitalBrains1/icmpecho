4a2f2358e320df6f42c4d4c7e2e20654e4d499f7
Peter Lebbing <peter@digitalbrains.com>
2024-05-14 18:42:06 +0200

    Gray code

--- src/IcmpEcho.hs
+++ src/IcmpEcho.hs
@@ -13,3 +13,33 @@ a ~+~ b = truncateB summed + resize (bitCoerce carry)
     where
         summed = a `add` b
         carry = msb summed
+
+f ::
+  Unsigned 4 ->
+  Unsigned 4
+f = decodeGray
+{-# ANN f (defSyn "f") #-}
+
+encodeGray ::
+  forall n .
+  KnownNat n =>
+  Unsigned n ->
+  Unsigned n
+encodeGray n = (n `shiftR` 1) `xor` n
+
+decodeGray ::
+  forall n .
+  KnownNat n =>
+  Unsigned (n+1) ->
+  Unsigned (n+1)
+decodeGray = go
+ where
+  go ::
+    forall m .
+    KnownNat m =>
+    Unsigned (m+1) ->
+    Unsigned (m+1)
+  go m =
+    case compareSNat (SNat @m) d0 of
+      SNatLE -> m
+      SNatGT -> m `xor` (extend $ go @(m-1) $ truncateB $ m `shiftR` 1)
